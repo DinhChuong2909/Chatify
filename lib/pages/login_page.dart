ad1c2a5a2322cc5031be4286e358f9b4576ec560
DinhChuong2909 <vudinhchuong2909@gmail.com>
2023-12-16 05:18:25 +0700

    Login UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// Widgets
+import '../widgets/custom_input_fields.dart';
+import '../widgets/rounded_button.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -13,6 +17,8 @@ class _LoginPageState extends State<LoginPage> {
   late double _deviceHeight;
   late double _deviceWidth;
 
+  final _loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -25,14 +31,26 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         padding: EdgeInsets.symmetric(
             horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
-        height: _deviceHeight,
-        width: _deviceWidth,
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             _pageTitle(),
+            SizedBox(
+              height: _deviceHeight * 0.04,
+            ),
+            _loginForm(),
+            SizedBox(
+              height: _deviceHeight * 0.05,
+            ),
+            _loginButton(),
+            SizedBox(
+              height: _deviceHeight * 0.05,
+            ),
+            _registerAccountLink(),
           ],
         ),
       ),
@@ -46,6 +64,54 @@ class _LoginPageState extends State<LoginPage> {
         'Chatify',
         style: TextStyle(
             color: Colors.white, fontSize: 40, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _deviceHeight * 0.18,
+      child: Form(
+          key: _loginFormKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomTextFormField(
+                  onSaved: (value) {},
+                  regEx:
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                  hintText: "Email",
+                  obscureText: false),
+              CustomTextFormField(
+                  onSaved: (value) {},
+                  regEx: r".{8,}",
+                  hintText: "Password",
+                  obscureText: true)
+            ],
+          )),
+    );
+  }
+
+  Widget _loginButton() {
+    return RoundedButton(
+        name: "Login",
+        height: _deviceHeight * 0.065,
+        width: _deviceWidth * 0.65,
+        onPressed: () {});
+  }
+
+  Widget _registerAccountLink() {
+    return GestureDetector(
+      onTap: () {
+        print("Clicked!");
+      },
+      child: Container(
+        child: const Text(
+          'Don\'t have an account?',
+          style: TextStyle(color: Colors.blueAccent),
+        ),
       ),
     );
   }
